6f0597f58dcc47a057fdd16a2f9e817609c9d7ef
rowenmark1 <60866008+rowenmark1@users.noreply.github.com>
2026-07-14 11:07:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Paste the deployed Google Apps Script URL between the quotes below.
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbypHtB4C4O-rUomrmrEUev-Py2aHgbQABJwCfDRLgdWZIOcp35H7HTCSIgG9GRAhMak/exec';
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzGyWsF3MA5pM5Cr3Kocwc7ZGqh1-4PZjNKaubUzYN4t4uzG-jegsc7wtuaJ8Z-qu0Eog/exec';
 
 const ENTRY_TYPES = {
   general: { label: 'General entry', price: 1950 },
